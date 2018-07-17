ac91230335e02a541671d9103616b89baf7ddb72
pjehan <pierre.jehan@gmail.com>
2018-07-17 21:43:59 +0200

    Add Redux data persistence

--- slideshow.config.js
+++ slideshow.config.js
@@ -1,4 +1,5 @@
 module.exports = {
     name: 'ReactJS',
-    stylesheets: ['assets/css/style.css']
+    stylesheets: ['assets/css/style.css'],
+    javascripts: ['https://ssl.gstatic.com/trends_nrtr/1480_RC02/embed_loader.js', 'assets/js/script.js']
 }
