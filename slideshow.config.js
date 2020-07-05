f918109734377b9a4f3a6648adc7daad7ff1d41a
pjehan <pierre.jehan@gmail.com>
2020-07-05 16:58:08 +0200

    Add hooks chapter

--- slideshow.config.js
+++ slideshow.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   name: 'ReactJS',
   stylesheets: [],
-  javascripts: ['assets/js/script.js']
+  javascripts: ['assets/js/script.js'],
+  revealjs: {
+    width: '100%',
+    height: '100%'
+  }
 };
